6aa47c2375957ec10e8ae7d460157a45cf873326
faizhameed <faizftb@gmail.com>
2020-02-27 17:05:40 +0530

    django admin template overriding and static css files generating styles dynamically

--- polls/views.py
+++ polls/views.py
@@ -3,14 +3,20 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from .models import Choice, Question
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+                                # """
+                                # Return the last five published questions (not including those set to be
+                                # published in the future).
+                                # """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
