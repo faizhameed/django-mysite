cc7098456df98cdea6bb2d1c43f12538c0070e6e
faizhameed <faizftb@gmail.com>
2020-03-03 14:22:36 +0530

    added django template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Experience
 
 
 def index(request):
-    return HttpResponse("Welcome to my portfolio")
+    explist = Experience.objects.all()
+    template = loader.get_template('portfolio/index.html')
+    context = {
+        'explist': explist
+    }
+    return HttpResponse(template.render(context, request))
